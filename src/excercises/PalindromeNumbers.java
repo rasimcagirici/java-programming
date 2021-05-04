930c9280234173be2867ead348ef9533ee80e074
Rasim Cagirici <rasimcagirici@gmail.com>
2021-05-04 18:29:56 -0400

    commit PalindromeNumbers

--- src/excercises/PalindromeNumbers.java
+++ src/excercises/PalindromeNumbers.java
@@ -2,7 +2,21 @@ package excercises;
 
 public class PalindromeNumbers {
     public static void main(String[] args) {
-        int num = 1221;
+
+        int num = 1321;
+        int temp = num;
+        int reverse = 0;
+        boolean isPalindrome = false;
+        while (temp != 0) {
+            int lastDigit;
+            lastDigit = temp % 10;
+            reverse = (reverse * 10) + lastDigit;
+            temp = temp / 10;
+        }
+        if (reverse == num) {
+            isPalindrome = true;
+        }
+        System.out.println(isPalindrome);
 
     }
 }
