fd91cb924085118d84fe1e6fa4a43b757e5f2a35
Rasim Cagirici <rasimcagirici@gmail.com>
2021-06-20 12:30:11 -0400

    day 55 abstraction

---
+++ src/day55_abstraction/exercise_example/Running.java
@@ -0,0 +1,15 @@
+package day55_abstraction.exercise_example;
+
+public class Running extends Exercise {
+
+    @Override
+    public void perform() {
+        System.out.println("Performing running exercise");
+
+    }
+
+    @Override
+    public int getCaloriesCount(int minutes) {
+        return minutes * 13;
+    }
+}
