31197dbf5008340203138cd83ede9d7151d18d9a
Rasim Cagirici <rasimcagirici@gmail.com>
2021-06-22 21:29:10 -0400

    day 56 abstraction

---
+++ src/day56_abstraction/drivable/Plane.java
@@ -0,0 +1,34 @@
+package day56_abstraction.drivable;
+
+import day56_abstraction.greeting.Greeting;
+
+public class Plane extends Transportation implements SelfDrivable, Greeting {
+
+    @Override
+    public void autoPiloting() {
+        System.out.println("Plane will be going on auto pilot");
+    }
+
+    @Override
+    public void transportPeople() {
+        System.out.println("Flying to people to desired destination");
+
+    }
+
+    @Override
+    public void cost(int mile) {
+        System.out.println("Plane cost " + (mile * 25.0) + " to fly " + mile + " miles");
+    }
+
+    @Override
+    public void hi() {
+        System.out.println("Welcome aboard");
+
+    }
+
+    @Override
+    public void bye() {
+        System.out.println("Thanks for flying with us");
+    }
+
+}
