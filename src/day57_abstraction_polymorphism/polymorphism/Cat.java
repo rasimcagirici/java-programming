56ba8f18255f239ddd52931ef603234db740af2e
Rasim Cagirici <rasimcagirici@gmail.com>
2021-06-27 15:34:37 -0400

    day 57 abstraction and polymorphism

--- src/day57_abstraction_polymorphism/polymorphism/Cat.java
+++ src/day57_abstraction_polymorphism/polymorphism/Cat.java
@@ -1,4 +1,10 @@
 package day57_abstraction_polymorphism.polymorphism;
 
 public class Cat extends Animal {
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Meow meow");
+    }
+
 }
--- src/day57_abstraction_polymorphism/polymorphism/Dog.java
+++ src/day57_abstraction_polymorphism/polymorphism/Dog.java
@@ -1,4 +1,10 @@
 package day57_abstraction_polymorphism.polymorphism;
 
 public class Dog extends Animal {
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Woof woof");
+    }
+
 }
