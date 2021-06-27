f1c2ec864714b0cba7cef03e8f4ea23e11672603
Rasim Cagirici <rasimcagirici@gmail.com>
2021-06-27 12:18:05 -0400

    day 57 abstraction and polymorphism

--- src/day57_abstraction_polymorphism/abstract_class_vs_interface/AbstractA.java
+++ src/day57_abstraction_polymorphism/abstract_class_vs_interface/AbstractA.java
@@ -2,4 +2,14 @@ package day57_abstraction_polymorphism.abstract_class_vs_interface;
 
 public abstract class AbstractA {
 
+    public static void helloM() {
+        System.out.println("Hello");
+    }
+
+    public abstract void abstractA();
+
+    public String helloN() {
+        return "Hello N";
+    }
+
 }
--- src/day57_abstraction_polymorphism/abstract_class_vs_interface/ConcreteA.java
+++ src/day57_abstraction_polymorphism/abstract_class_vs_interface/ConcreteA.java
@@ -1,4 +1,9 @@
 package day57_abstraction_polymorphism.abstract_class_vs_interface;
 
-public class ConcreteA extends AbstractA{
+public class ConcreteA extends AbstractA implements InterfaceA, InterfaceB {
+
+    @Override
+    public void abstractA() {
+
+    }
 }
