0af97caa96b26de00b481a687e7d002e12d36e4d
Rasim Cagirici <rasimcagirici@gmail.com>
2021-06-29 20:47:03 -0400

    day 58 polymorphism

---
+++ src/day58_polymorphism/SuperManTest.java
@@ -0,0 +1,17 @@
+package day58_polymorphism;
+
+public class SuperManTest {
+
+    public static void main(String[] args) {
+
+        Father spMan1 = new SuperMan();
+        spMan1.playWithKid();
+        spMan1.feedKid();
+        spMan1.raiseKid();
+
+        Worker spMan2 = new SuperMan();
+        spMan2.work("SDET");
+        spMan2.getPaid();
+    }
+
+}
