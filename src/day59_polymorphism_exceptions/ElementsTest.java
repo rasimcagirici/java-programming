b41f69cb6e66e1549da9057440cb6379257dcb24
Rasim Cagirici <rasimcagirici@gmail.com>
2021-07-03 14:21:18 -0400

    day 59 polymorphism and exceptions

--- src/day59_polymorphism_exceptions/ElementsTest.java
+++ src/day59_polymorphism_exceptions/ElementsTest.java
@@ -2,4 +2,27 @@ package day59_polymorphism_exceptions;
 
 public class ElementsTest {
 
+    public static void main(String[] args) {
+
+        Link link = new Link();
+        link.click();
+        link.sendKeys("Hi");
+        link.getText();
+        link.getLinkHref();
+
+        WebElement link1 = new Link();
+        link1.click();
+        link1.sendKeys("Hello");
+        link1.getText();
+
+        WebElement username = new InputField();
+
+        WebElementUtil.clickElement(link1);
+        WebElementUtil.clickElement(username);
+
+        WebElement login = WebElementUtil.getLinkWithText("Hello");
+        login.click();
+
+    }
+
 }
