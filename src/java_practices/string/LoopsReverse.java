abde284ca2e01b85431ab2622d3cb5be1621ac35
Rasim Cagirici <rasimcagirici@gmail.com>
2021-07-04 21:07:44 -0400

    core java practices - loop practice

--- src/java_practices/string/LoopsReverse.java
+++ src/java_practices/string/LoopsReverse.java
@@ -6,28 +6,36 @@ public class LoopsReverse {
 
         int count = 1;
 
-        //for (int i = 4; i > 0; i--) {
+        for (int i = 4; i > 0; i--) {
 
-            //System.out.print(count++ + " ");
+            System.out.print(count++ + " ");
 
-           // for (int j = i; j < 4; j++) {
+           for (int j = i; j < 4; j++) {
 
-               // System.out.print(count++ + " ");
+               System.out.print(count++ + " ");
 
-           // }
+           }
 
-          //  System.out.println();
+          System.out.println();
 
-       // }
+       }
 
         for (int i = 1; i < 5; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(count++);
+                System.out.print(j);
                 System.out.print("\t");
             }
             System.out.println();
         }
 
-    }
+        int counter = 0;
 
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter+=3);
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
 }
