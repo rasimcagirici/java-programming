d0e2ec7836ddfaf0d5ab4781fbb2e7515dbf6879
Rasim Cagirici <rasimcagirici@gmail.com>
2021-07-11 15:31:08 -0400

    day 61 collections and exceptions

---
+++ src/day61_exceptions_collections/checked_exceptions/CheckExceptionDemo.java
@@ -0,0 +1,9 @@
+package day61_exceptions_collections.checked_exceptions;
+
+public class CheckExceptionDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread.sleep(1000);
+    }
+
+}
