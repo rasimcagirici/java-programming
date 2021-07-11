b83f82b929ef26a277fd8aecc97b4d12f8ba3f6e
Rasim Cagirici <rasimcagirici@gmail.com>
2021-07-11 15:47:26 -0400

    day 61 collections and exceptions

--- src/day61_exceptions_collections/checked_exceptions/CheckExceptionDemo.java
+++ src/day61_exceptions_collections/checked_exceptions/CheckExceptionDemo.java
@@ -3,7 +3,9 @@ package day61_exceptions_collections.checked_exceptions;
 public class CheckExceptionDemo {
 
     public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(1000);
+        System.out.println("About to sleep for 5 seconds");
+        Thread.sleep(5000);
+        System.out.println("Woke up after 5 seconds");
     }
 
 }
